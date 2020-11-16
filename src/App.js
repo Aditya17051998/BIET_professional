b63e380007ba5f77013b87399ef1ceeadefe45af
Aditya Kushwaha <kushwahaa862@gmail.com>
2020-11-16 13:12:52 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <div className="App">
+      biet professional
+    </div>
+  );
+}
+
+export default App;
